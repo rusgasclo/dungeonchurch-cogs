0a5688efb5de7790d30dc65178afa8a22d46527b
oakbrad <brad@bradwenner.com>
2024-11-12 19:25:12 -0800

    [churchmod] cog_check to only run on our servers #15

--- churchmod/churchmod.py
+++ churchmod/churchmod.py
@@ -1,4 +1,5 @@
 from redbot.core import commands
+import discord
 from .dm_lib import church_channels
 
 class ChurchMod(commands.Cog):
@@ -9,3 +10,21 @@ class ChurchMod(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.server_id = [1190404189214494800, 828777456898277396]
+
+    # Cog check to make sure we only run on DC servers
+    async def cog_check(self, ctx: commands.Context) -> bool:
+        """Restrict cog commands to our servers."""
+        if ctx.guild is not None and ctx.guild.id in self.server_id:
+            return True
+        await ctx.send("> *This accursed place is not my [home](https://www.dungeon.church)!*\n--**The Deacon**")
+        return False
+    
+    # 
+    # Command group for churchmod
+    #
+    @commands.group()
+    async def churchmod(self, ctx):
+        """Commands for managing churchmod automation."""
+        pass
+
