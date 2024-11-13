d9855a0b05c50f05a5efee8395b1563ba13766ca
oakbrad <brad@bradwenner.com>
2024-11-12 21:24:54 -0800

    [churchmod] Fix guild config, msg formatting

--- churchmod/churchmod.py
+++ churchmod/churchmod.py
@@ -1,4 +1,5 @@
 from redbot.core import commands, Config, checks
+from redbot.core.utils.chat_formatting import error, question, success
 import discord
 from .dm_lib import church_channels
 
@@ -11,9 +12,14 @@ class ChurchMod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.server_id = [1190404189214494800, 828777456898277396] # [dev, prod]
-        default_guild_settings = {
+        self.config = Config.get_conf(
+            self, identifier=4206661045, force_registration=True
+        )
+        default_guild = {
             "debug_mode": False,
         }
+        self.config.register_guild(**default_guild)
+
     #
     # Bot functions
     #
@@ -21,14 +27,14 @@ class ChurchMod(commands.Cog):
         """Restrict cog commands to our servers."""
         if ctx.guild is not None and ctx.guild.id in self.server_id:
             return True
-        await ctx.send("> *This accursed place is not my [home](https://www.dungeon.church)!*\n--**The Deacon**")
+        await ctx.send(error("*This accursed place is not my [home](https://www.dungeon.church)!*\n--**The Deacon**"))
         return False
     
     async def _channel(self, channel_name: str, ctx) -> int:
         """Returns the channel ID based on the environment and debug mode."""
         if ctx.guild.id == self.server_id[0]:
             return church_channels["dev-server"]
-        debug_mode = await self.config.debug_mode() 
+        debug_mode = await self.config.guild.debug_mode() 
         if not debug_mode:
             return church_channels.get(channel_name, church_channels["server-log"])
         return church_channels["server-log"]
@@ -44,15 +50,15 @@ class ChurchMod(commands.Cog):
 
     @churchmod.command()
     async def debug(self, ctx: commands.Context, state: bool) -> None:
-        """Toggle debug mode on or off in prod. Dev server, do nothing."""
+        """Toggle debug mode on or off in prod. """
         if ctx.guild.id == self.server_id[0]:
-            await ctx.send("`You can't do that here.`")
+            await ctx.send(error("`You can't do that here.`"))
             return
-        current_state = await self.config.debug_mode()
+        current_state = await self.config.guild(ctx.guild).debug_mode()
         if state == current_state:
-            await ctx.send(f"`Debug mode is already {'on' if current_state else 'off'}.`")
+            await ctx.send(error(f"`Debug mode is already {'on' if current_state else 'off'}.`"))
             return
-        await self.config.debug_mode.set(state)
-        await ctx.send(f"`Debug mode was turned {'on' if state else 'off'}.`")
+        await self.config.guild(ctx.guild).debug_mode.set(state)
+        await ctx.send(success(f"`Debug mode was turned {'on' if state else 'off'}.`"))
         return
     
