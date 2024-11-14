a2c800e8e6975561c4df6a6944012d10c2c3c467
oakbrad <brad@bradwenner.com>
2024-11-14 00:09:04 -0800

    [churchmod] better make_npc

--- churchmod/mod.py
+++ churchmod/mod.py
@@ -10,6 +10,4 @@ from .dm_lib import church_roles, church_channels
 async def name_npc(member: discord.Member) -> None:
     """Append ` || NPC` to server nickname"""
     if "||" not in (member.nick or member.display_name):
-        # Update the nickname to "Name || NPC"
-        new_nickname = f"{member.display_name} || NPC"
-        await member.edit(nick=new_nickname)
+        await member.edit(nick=f"{member.display_name} || NPC")
